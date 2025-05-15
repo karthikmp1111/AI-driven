3955fc3a4d0df0bd6a2ccf8aa8fa773cebd5d1bd
karthikeyan <karthikeyan@bicsglobal.com>
2025-05-15 12:13:20 +0530

    testing1

--- lambda-functions/lambda/build.sh
+++ lambda-functions/lambda/build.sh
@@ -1,12 +1,25 @@
+# #!/bin/bash
+# set -e
+
+# # Cleanup previous package
+# rm -rf python lambda_function.zip
+
+# # Install dependencies
+# pip install -r requirements.txt -t python/
+
+# # Zip dependencies and your function
+# cd python && zip -r9 ../lambda_function.zip . && cd ..
+# zip -g lambda_function.zip lambda_function.py
+
 #!/bin/bash
 set -e
+
+cd "$(dirname "$0")"
 
 rm -rf python package.zip
 
 pip install -r requirements.txt -t python/
 
-cd python
-zip -r9 ../package.zip .
-cd ..
+zip -r9 package.zip python/
 
 zip -g package.zip lambda_function.py
